6d9125afb5462c97409c24ae83d5d2cd14d2b040
绝世好男人 <422375723@qq.com>
2019-07-01 23:57:12 +0800

    s

--- src/components/theme/login/login.vue
+++ src/components/theme/login/login.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="login" class="loginDiv">
-    <div class="inner" v-if="loginType==='ordinary'">
-      <div class="inner-div">
-        <label class="inner-div-label">账号:</label>
-        <input class="inner-div-input" type="text" v-model="ordinary.username">
-      </div>
-      <div class="inner-div">
-        <label class="inner-div-label">密码:</label>
-        <input class="inner-div-input" type="text" v-model="ordinary.password">
-      </div>
-      <div class="inner-div">
-        <input type="button" value="登陆" @click="toHomePage">
-        <input type="button" value="取消">
-      </div>
-    </div>
-    <div class="inner" v-if="loginType==='oauth'||loginType==='security'">
-
+    <div class="inner">
+      <el-card class="box-card">
+        <div  class="text item">
+          <div class="inner-div">
+            <label class="inner-div-label">账号:</label>
+            <el-input v-model="input" placeholder="请输入账号"></el-input>
+          </div>
+        </div>
+        <div  class="text item">
+          <div class="inner-div">
+            <label class="inner-div-label">密码:</label>
+            <el-input v-model="input" placeholder="请输入密码"></el-input>
+          </div>
+        </div>
+        <div  class="text item">
+          <div class="inner-div">
+            <el-button type="primary" plain size="medium">登录</el-button>
+          </div>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -23,7 +27,6 @@
   import Vue from 'vue'
   import { Component } from 'vue-property-decorator'
   import { namespace } from 'vuex-class'
-  import store from '@/vuex'
 
   const loginModel = namespace('Login')
  @Component
@@ -35,38 +38,24 @@
     ordinaryUrl: ''
   }
   oauth = {
-     'grant_type': 'code',
+     'grant_type': 'password',
      'client_id': 'ownerapp',
      'redirect_uri': '',
-     'oauthServerUrl':''
+     'oauthServerUrl': ''
    }
    @loginModel.Action('login')
    login
-   @loginModel.Action('oauthJump')
-   oauthJump
-   @loginModel.Action('securityJump')
-   securityJump
-
-  jump () {
-      if (this.loginType === 'oauth') {
-        this.oauthJump(this.oauth)
-      }else if (this.loginType === 'security'){
-        this.securityJump(this.security)
-      }
-    }
   toHomePage () {
-    this.login(ordinary).then(ele => {
-       store.commit('loginType',this.loginType)
-       store.commit('updateUser',ele.data())
-
-    })
-
   }
   created () {
-    this.jump()
+
   }
   }
 </script>
 <style>
   @import "./login.css";
+  .text {
+    font-size: 18px;
+  }
+
 </style>
